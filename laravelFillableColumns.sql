1916d82b11b4f25b8b9586f59198df8e46896e22
Simon Rogers <simon@blue.midnite.co.uk>
2016-02-25 09:34:02 +0000

    Update laravel fillable
    Update so the db selected is the current one by default and column names outputted have quote marks.

--- laravelFillableColumns.sql
+++ laravelFillableColumns.sql
@@ -1,15 +1,19 @@
 -- +------------------------------------------------------
 -- + Retrieve column names to add to the fillable array in Laravel
 -- + Author: Simon Rogers
--- + Date: 10th February 2016
+-- + Created Date: 10th February 2016
+-- + Modified Date: 25th February 2016
 -- + SQL: mysql
 -- +------------------------------------------------------
 
 -- +------------------------------------------------------
 -- + Set the database you want to use
+-- + Change database() to a string if you want to use 
+-- + another database other than the current one
+-- + e.g SET @db = 'myOtherDatabase'; 
 -- +------------------------------------------------------
 
-SET @db = 'myDb'; 
+SET @db = database(); 
 
 -- +------------------------------------------------------
 -- + Increase the group_concat max length so you don't 
@@ -23,7 +27,7 @@ SET SESSION group_concat_max_len = 1000000;
 -- + deleted_at as by default these would not be fillable
 -- +------------------------------------------------------
 
-SELECT table_name, group_concat(column_name ORDER BY `ORDINAL_POSITION` SEPARATOR ', ') as `Columns` 
+SELECT table_name, group_concat(concat('\'',column_name,'\'') ORDER BY `ORDINAL_POSITION` SEPARATOR ', ') as `Columns` 
 FROM information_schema.columns 
 WHERE table_schema = @db and column_name NOT IN ('id', 'created_at', 'updated_at', 'deleted_at')
 GROUP BY table_name;
