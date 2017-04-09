bcbdc663c034a6f688863dcd30f5937b5bdb4c6c
Simon Rogers <midnite81@users.noreply.github.com>
2017-04-09 19:37:38 +0100

    Create deleteAllTables.sql

---
+++ deleteAllTables.sql
@@ -0,0 +1,18 @@
+-- +------------------------------------------------------
+-- + Generates SQL to delete all tables from the current active database
+-- + Author: Simon Rogers
+-- + Created Date: 9th April 2017
+-- + SQL: mysql
+-- +------------------------------------------------------
+
+SELECT 'SET FOREIGN_KEY_CHECKS = 0;' as `SQL`
+
+UNION ALL 
+
+SELECT concat('DELETE TABLE `', TABLE_NAME, '`;') as `SQL` 
+FROM information_schema.tables
+WHERE TABLE_SCHEMA = database()
+
+UNION ALL 
+
+SELECT 'SET FOREIGN_KEY_CHECKS = 1;' as `SQL`;
